1f65a2f563127d6b0d07e18bafcfde5610ca18bc
Nguyen Thanh Binh <binh15122003@gmail.com>
2023-03-27 21:01:24 +0700

    update

---
+++ src/main/java/lap_trinh_java/sieuthi_mini_java/Sieuthi_mini_java.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package lap_trinh_java.sieuthi_mini_java;
+
+/**
+ *
+ * @author chris
+ */
+public class Sieuthi_mini_java {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
