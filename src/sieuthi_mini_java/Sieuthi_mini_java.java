45dcad2be2305e70542119bf43cfd5620754c488
Nguyen Thanh Binh <69106519+notobo15@users.noreply.github.com>
2023-03-27 16:11:13 +0700

    update

---
+++ src/sieuthi_mini_java/Sieuthi_mini_java.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package sieuthi_mini_java;
+
+import GUI.AlertMessageYN;
+
+/**
+ *
+ * @author notobo
+ */
+public class Sieuthi_mini_java {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new AlertMessageYN("xin chao").setVisible(true);
+    }
+    
+}
